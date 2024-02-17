a6bb75de77d22468ff5aca58773cf60ddb3fbaa5
Yasser Sallam <ysallam.c@solutions.com.sa>
2024-02-17 15:13:38 +0300

    Refactor extensions

--- src/Apis/Common/HealthCheckDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Apis.Common;
-
-public class HealthCheckDto
-{
-    public string Status { get; set; }
-    public int StatusCode { get; set; }
-}
--- src/Apis/Common/IAnonymousEndpointsService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace Apis.Common;
-
-public interface IAnonymousEndpointsService
-{
-    bool IsAnonymous(HttpContext context);
-}
